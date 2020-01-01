b906e1db2a529b1faa037e5e6e7c99d6aa52a1e8
respawn_sato <sato@respawn.tokyo>
2020-01-01 22:14:01 +0900

    お試しコミット:ディレクトリ作成～仮公開まで

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var $gradientStop1 = document.querySelector('#js-gradient__stop-1');
+var $gradientStop2 = document.querySelector('#js-gradient__stop-2');
+
+window.addEventListener('scroll', function() {
+  var scrollTop = window.pageYOffset;
+  var val = scrollTop / 10;
+  var hue1 = (val) % 360;
+  var hue2 = (val + 60) % 360;
+  
+  TweenMax.set($gradientStop1, {
+    stopColor: 'hsl('+hue1+', 100%, 60%)'
+  });
+
+  TweenMax.set($gradientStop2, {
+    stopColor: 'hsl('+hue2+', 100%, 60%)'
+  });
+});
+
+// wavifyAnimation
+
+/** jQueryの処理 */
+
+
+jQuery(function($){
+    // コード
+    jQuery('#waves').wavify({
+        height: 100,
+        bones: 4,
+        amplitude: 40,
+        color: '#f0f8ff',
+        speed: .25
+    });
+});
