1cf9f04f29972611b81d14c5a83f47990afdb0f8
respawn_sato <sato@respawn.tokyo>
2020-08-11 17:05:23 +0900

    HTMLJS：ランダム表示機能を追加

---
+++ js/song.list.js
@@ -0,0 +1,258 @@
+// 各時代ごとにランダムで表示させるロジック
+$(function(){
+    var x;
+    var h1Songs =[];
+    var h2Songs =[];
+    var h3Songs =[];
+    var h4Songs =[];
+    var h5Songs =[];
+    var h6Songs =[];
+    var h7Songs =[];
+    var h8Songs =[];
+    var h9Songs =[];
+    var h10Songs =[];
+    var h11Songs =[];
+    var h12Songs =[];
+    var h13Songs =[];
+    var h14Songs =[];
+    var h15Songs =[];
+    var h16Songs =[];
+    var h17Songs =[];
+    var h18Songs =[];
+    var h19Songs =[];
+    var h20Songs =[];
+    var h21Songs =[];
+    var h22Songs =[];
+    var h23Songs =[];
+    var h24Songs =[];
+    var h25Songs =[];
+    var h26Songs =[];
+    var h27Songs =[];
+    var h28Songs =[];
+    var h29Songs =[];
+    var h30Songs =[];
+    var h31Songs =[];
+    x = Math.floor(Math.random()*3);
+
+    // 1989年
+    h1Songs[0] = '<a href="https://youtu.be/nGl2la0Q6LA" data-lity>未来予想図Ⅱ / <br class="pc-dnone">DREAMS COME TRUE</a>';
+    h1Songs[1] = '<a href="https://youtu.be/iBDnRK9ACTY" data-lity>淋しい熱帯魚 / <br class="pc-dnone">Wink</a>';
+    h1Songs[2] = '<a href="https://youtu.be/9vEw-r3mmfk" data-lity>大迷惑 / <br class="pc-dnone">ユニコーン</a>';
+
+    h1Song = document.getElementById("h1Song");
+    h1Song.innerHTML = h1Songs[x];
+
+    // 1990年
+    h2Songs[0] = '<a href="https://youtu.be/8PdjoVJxDmY" data-lity>さよならを待ってる / <br class="pc-dnone">DREAMS COME TRUE</a>';
+    h2Songs[1] = '<a href="https://youtu.be/C7QVaahn44A" data-lity>壊れかけのRadio / <br class="pc-dnone">德永英明</a>';
+    h2Songs[2] = '<a href="https://youtu.be/F-Oax86SCuo" data-lity>惡の華 / <br class="pc-dnone">BUCK-TICK</a>';
+
+    h2Song = document.getElementById("h2Song");
+    h2Song.innerHTML = h2Songs[x];
+
+    // 1991年
+    h3Songs[0] = '<a href="https://youtu.be/nGl2la0Q6LA" data-lity>はじまりはいつも雨 / <br class="pc-dnone">ASKA</a>';
+    h3Songs[1] = '<a href="https://youtu.be/Q9qAyt0G-jM" data-lity>SAY YES / <br class="pc-dnone">CHAGE and ASKA</a>';
+    h3Songs[2] = '<a href="https://youtu.be/b88pxLpMZKk" data-lity>どんなときも。 / <br class="pc-dnone">槇原敬之</a>';
+
+    h3Song = document.getElementById("h3Song");
+    h3Song.innerHTML = h3Songs[x];
+
+    // 1992年
+    h4Songs[0] = '<a href="https://youtu.be/O1UVEwMADQ8" data-lity>決戦は金曜日 / <br class="pc-dnone">DREAMS COME TRUE</a>';
+    h4Songs[1] = '<a href="https://youtu.be/naz0-szzYXk" data-lity>もう恋なんてしない / <br class="pc-dnone">槇原敬之</a>';
+    h4Songs[2] = '<a href="https://youtu.be/5WYHAaUlzpw" data-lity>WON’T BE LONG / <br class="pc-dnone">バブルガム・ブラザーズ</a>';
+
+    h4Song = document.getElementById("h4Song");
+    h4Song.innerHTML = h4Songs[x];
+
+    // 1993年
+    h5Songs[0] = '<a href="https://youtu.be/WzKI-Q7B8Xo" data-lity>MELODY / <br class="pc-dnone">福山雅治</a>';
+    h5Songs[1] = '<a href="https://youtu.be/NCPH9JUFESA" data-lity>負けないで / <br class="pc-dnone">ZARD</a>';
+    h5Songs[2] = '<a href="https://youtu.be/yfZIaTZJo0o" data-lity>YAH YAH YAH / <br class="pc-dnone">CHAGE and ASKA</a>';
+
+    h5Song = document.getElementById("h5Song");
+    h5Song.innerHTML = h5Songs[x];
+
+    // 1994年
+    h6Songs[0] = '<a href="https://youtu.be/DRLkiX-NZzE" data-lity>ROSIER / <br class="pc-dnone">LUNA SEA</a>';
+    h6Songs[1] = '<a href="https://youtu.be/rfVhsgCqG0I" data-lity>innocent world / <br class="pc-dnone">Mr.Children</a>';
+    h6Songs[2] = '<a href="https://youtu.be/qX7pFYH9O04" data-lity>春よ、来い / <br class="pc-dnone">松任谷由実</a>';
+    h6Song = document.getElementById("h6Song");
+    h6Song.innerHTML = h6Songs[x];
+
+    // 1995年
+    h7Songs[0] = '<a href="https://youtu.be/bplLbHAGSAM" data-lity>ズルい女 / <br class="pc-dnone">シャ乱Q</a>';
+    h7Songs[1] = '<a href="https://youtu.be/RkF5_BpBQU4" data-lity>Hello, Again 〜昔からある場所〜 / <br class="pc-dnone">My Little Lover</a>';
+    h7Songs[2] = '<a href="https://youtu.be/ACt_DfVjqIY" data-lity>シーソーゲーム ～勇敢な恋の歌～ /<br class="pc-dnone">Mr.Children</a>';
+    h7Song = document.getElementById("h7Song");
+    h7Song.innerHTML = h7Songs[x];
+
+    // 1996年
+    h8Songs[0] = '<a href="https://youtu.be/8RXUBBKAIs0" data-lity>これが私の生きる道 / <br class="pc-dnone">PUFFY</a>';
+    h8Songs[1] = '<a href="https://youtu.be/Mg7rtAWKdok" data-lity>イージュー★ライダー / <br class="pc-dnone">奥田民生</a>';
+    h8Songs[2] = '<a href="https://youtu.be/51CH3dPaWXc" data-lity>ロビンソン / <br class="pc-dnone">スピッツ</a>';
+    h8Song = document.getElementById("h8Song");
+    h8Song.innerHTML = h8Songs[x];
+
+    // 1997年
+    h9Songs[0] = '<a href="https://youtu.be/k1S0i8FoV4c" data-lity>出逢った頃のように / <br class="pc-dnone">Every Little Thing</a>';
+    h9Songs[1] = '<a href="https://youtu.be/0melyLHqP-s" data-lity>今宵の月のように / <br class="pc-dnone">エレファントカシマシ</a>';
+    h9Songs[2] = '<a href="https://youtu.be/gPcPseeICjs" data-lity>HOWEVER / <br class="pc-dnone">GLAY</a>';
+    h9Song = document.getElementById("h9Song");
+    h9Song.innerHTML = h9Songs[x];
+
+    // 1998年
+    h10Songs[0] = '<a href="https://youtu.be/Hpk-ZqQpq7Y" data-lity>サマーナイトタウン / <br class="pc-dnone">モーニング娘。</a>';
+    h10Songs[1] = '<a href="https://youtu.be/fGlDHwlq5SQ" data-lity>誘惑 / <br class="pc-dnone">GLAY</a>';
+    h10Songs[2] = '<a href="https://youtu.be/PwhKGjv4Yjc" data-lity>Time goes by / <br class="pc-dnone">Every Little Thing</a>';
+    h10Song = document.getElementById("h10Song");
+    h10Song.innerHTML = h10Songs[x];
+
+    // 1999年
+    h11Songs[0] = '<a href="https://youtu.be/8yHS-AEHijs" data-lity>Be cool!! / <br class="pc-dnone">野猿</a>';
+    h11Songs[1] = '<a href="https://youtu.be/-9DxpPiE458" data-lity>Automatic / <br class="pc-dnone">宇多田ヒカル</a>';
+    h11Songs[2] = '<a href="https://youtu.be/Vxmxpbn0pwA" data-lity>アポロ / <br class="pc-dnone">ポルノグラフィティ</a>';
+    h11Song = document.getElementById("h11Song");
+    h11Song.innerHTML = h11Songs[x];
+
+    // 2000年
+    h12Songs[0] = '<a href="https://youtu.be/iyw6-KVmgow" data-lity>月光 / <br class="pc-dnone">鬼束ちひろ</a>';
+    h12Songs[1] = '<a href="https://youtu.be/AuZSyBa6phU" data-lity>SEASONS / <br class="pc-dnone">浜崎あゆみ</a>';
+    h12Songs[2] = '<a href="https://youtu.be/v2SlpjCz7uE" data-lity>地上の星 / <br class="pc-dnone">中島みゆき</a>';
+    h12Song = document.getElementById("h12Song");
+    h12Song.innerHTML = h12Songs[x];
+
+    // 2001年
+    h13Songs[0] = '<a href="https://youtu.be/fY1xN13G0pk" data-lity>サウダージ / <br class="pc-dnone">ポルノグラフィティ</a>';
+    h13Songs[1] = '<a href="https://youtu.be/rpFUNMmKIlg" data-lity>if... / <br class="pc-dnone">DA PUMP</a>';
+    h13Songs[2] = '<a href="https://youtu.be/j7CDb610Bg0" data-lity>天体観測 / <br class="pc-dnone">BUMP OF CHICKEN</a>';
+    h13Song = document.getElementById("h13Song");
+    h13Song.innerHTML = h13Songs[x];
+
+    // 2002年
+    h14Songs[0] = '<a href="https://youtu.be/7MEsNC-MdRE" data-lity>Fantasista / <br class="pc-dnone">Dragon Ash</a>';
+    h14Songs[1] = '<a href="https://youtu.be/tuyZ9f6mHZk" data-lity>traveling / <br class="pc-dnone">宇多田ヒカル</a>';
+    h14Songs[2] = '<a href="https://youtu.be/mq9dl6hw_yk" data-lity>楽園ベイベー / <br class="pc-dnone">RIP SLYME</a>';
+    h14Song = document.getElementById("h14Song");
+    h14Song.innerHTML = h14Songs[x];
+
+    // 2003年
+    h15Songs[0] = '<a href="https://youtu.be/F9L7QAL5m5g" data-lity>sailing day / <br class="pc-dnone">BUMP OF CHICKEN</a>';
+    h15Songs[1] = '<a href="https://youtu.be/k4YJcOrefEg" data-lity>ビバ★ロック / <br class="pc-dnone">ORANGE RANGE</a>';
+    h15Songs[2] = '<a href="https://youtu.be/SXyqhjhaQQA" data-lity>虹 / <br class="pc-dnone">福山雅治</a>';
+    h15Song = document.getElementById("h15Song");
+    h15Song.innerHTML = h15Songs[x];
+
+    // 2004年
+    h16Songs[0] = '<a href="https://youtu.be/SC6fWmtzGf0" data-lity>スターゲイザー / <br class="pc-dnone">スピッツ</a>';
+    h16Songs[1] = '<a href="https://youtu.be/cr8magEp0Ho" data-lity>リライト / <br class="pc-dnone">ASIAN KUNG-FU GENERATION</a>';
+    h16Songs[2] = '<a href="https://youtu.be/gD2mhJ3ByGQ" data-lity>群青日和 / <br class="pc-dnone">東京事変</a>';
+    h16Song = document.getElementById("h16Song");
+    h16Song.innerHTML = h16Songs[x];
+
+    // 2005年
+    h17Songs[0] = '<a href="https://youtu.be/gRqwgr-RXaY" data-lity>さくら / <br class="pc-dnone">ケツメイシ</a>';
+    h17Songs[1] = '<a href="https://youtu.be/rCNvayKkpYI" data-lity>New World / <br class="pc-dnone">L’Arc～en～Ciel</a>';
+    h17Songs[2] = '<a href="https://youtu.be/ZGf0nJhMHbE" data-lity>Dreamland / <br class="pc-dnone">BENNIE K</a>';
+    h17Song = document.getElementById("h17Song");
+    h17Song.innerHTML = h17Songs[x];
+
+    // 2006年
+    h18Songs[0] = '<a href="https://youtu.be/zgLAWm68XVg" data-lity>何度でも / <br class="pc-dnone">DREAMS COME TRUE</a>';
+    h18Songs[1] = '<a href="https://youtu.be/wHw6W4BznTM" data-lity>三日月 / <br class="pc-dnone">絢香</a>';
+    h18Songs[2] = '<a href="https://youtu.be/-Uf5ZhHDKsE" data-lity>Salamander / <br class="pc-dnone">ELLEGARDEN</a>';
+    h18Song = document.getElementById("h18Song");
+    h18Song.innerHTML = h18Songs[x];
+
+    // 2007年
+    h19Songs[0] = '<a href="https://youtu.be/PjGbnPYwt1g" data-lity>睡蓮花 / <br class="pc-dnone">湘南乃風</a>';
+    h19Songs[1] = '<a href="https://youtu.be/EAgACSowE5k" data-lity>Love so sweet / <br class="pc-dnone">嵐</a>';
+    h19Songs[2] = '<a href="https://youtu.be/jmKRgqWGrWc" data-lity>Beautiful World / <br class="pc-dnone">宇多田ヒカル</a>';
+    h19Song = document.getElementById("h19Song");
+    h19Song.innerHTML = h19Songs[x];
+
+    // 2008年
+    h20Songs[0] = '<a href="https://youtu.be/gU5oN0KVofU" data-lity>愛をこめて花束を / <br class="pc-dnone">Superfly</a>';
+    h20Songs[1] = '<a href="https://youtu.be/WzfteHcskYo" data-lity>POP STAR / <br class="pc-dnone">平井 堅</a>';
+    h20Songs[2] = '<a href="https://youtu.be/DwTinTO0o9I" data-lity>キセキ / <br class="pc-dnone">GReeeeN</a>';
+    h20Song = document.getElementById("h20Song");
+    h20Song.innerHTML = h20Songs[x];
+
+    // 2009年
+    h21Songs[0] = '<a href="https://youtu.be/7MaF-bWeLGw" data-lity>おしゃかしゃま / <br class="pc-dnone">RADWIMPS</a>';
+    h21Songs[1] = '<a href="https://youtu.be/cMsGcW-xaYU" data-lity>ワンルーム・ディスコ / <br class="pc-dnone">Perfume</a>';
+    h21Songs[2] = '<a href="https://youtu.be/H7Qi_-aKJzA" data-lity>明日がくるなら / <br class="pc-dnone">JUJU with JAY’ED</a>';
+    h21Song = document.getElementById("h21Song");
+    h21Song.innerHTML = h21Songs[x];
+
+    // 2010年
+    h22Songs[0] = '<a href="https://youtu.be/r3mIpbGD-xw" data-lity>Sunshine Girl / <br class="pc-dnone">moumoon</a>';
+    h22Songs[1] = '<a href="https://youtu.be/99zfIoCp3nk" data-lity>chAngE / <br class="pc-dnone">miwa</a>';
+    h22Songs[2] = '<a href="https://youtu.be/JedKBJVHhiE" data-lity>Monster / <br class="pc-dnone">嵐</a>';
+    h22Song = document.getElementById("h22Song");
+    h22Song.innerHTML = h22Songs[x];
+
+    // 2011年
+    h23Songs[0] = '<a href="https://youtu.be/K1u_MwT6zV8" data-lity>君と羊と青 / <br class="pc-dnone">RADWIMPS</a>';
+    h23Songs[1] = '<a href="https://youtu.be/tZbXHt3xPr8" data-lity>バッハの旋律を夜に聴いたせいです。 / <br class="pc-dnone">サカナクション</a>';
+    h23Songs[2] = '<a href="https://youtu.be/vEVq_Bx7_KY" data-lity>Everyday、カチューシャ / <br class="pc-dnone">AKB48</a>';
+    h23Song = document.getElementById("h23Song");
+    h23Song.innerHTML = h23Songs[x];
+
+    // 2012年
+    h24Songs[0] = '<a href="https://youtu.be/Hh9yZWeTmVM" data-lity>The Beginning / <br class="pc-dnone">ONE OK ROCK</a>';
+    h24Songs[1] = '<a href="https://youtu.be/MTcqIgVBfuk" data-lity>手をたたけ / <br class="pc-dnone">NICO Touches the Walls</a>';
+    h24Songs[2] = '<a href="https://youtu.be/NLy4cvRx7Vc" data-lity>つけまつける / <br class="pc-dnone">きゃりーぱみゅぱみゅ</a>';
+    h24Song = document.getElementById("h24Song");
+    h24Song.innerHTML = h24Songs[x];
+
+    // 2013年
+    h25Songs[0] = '<a href="https://youtu.be/teMdjJ3w9iM" data-lity>にんじゃりばんばん / <br class="pc-dnone">きゃりーぱみゅぱみゅ</a>';
+    h25Songs[1] = '<a href="https://youtu.be/NhI2mp-WIck" data-lity>Magic of Love / <br class="pc-dnone">Perfume</a>';
+    h25Songs[2] = '<a href="https://youtu.be/BC9P3DSZu0A" data-lity>女々しくて / <br class="pc-dnone">ゴールデンボンバー</a>';
+    h25Song = document.getElementById("h25Song");
+    h25Song.innerHTML = h25Songs[x];
+
+    // 2014年
+    h25Songs[2] = '<a href="https://youtu.be/rKsQ-3N-Bks" data-lity>ひまわりの約束 / <br class="pc-dnone">秦 基博</a>';
+    h26Songs[1] = '<a href="https://youtu.be/dFf4AgBNR1E" data-lity>恋するフォーチュンクッキー / <br class="pc-dnone">AKB48</a>';
+    h26Songs[2] = '<a href="https://youtu.be/p-RLC9ZgjhY" data-lity>NIPPON / <br class="pc-dnone">椎名林檎</a>';
+    h26Song = document.getElementById("h26Song");
+    h26Song.innerHTML = h26Songs[x];
+
+    // 2015年
+    h27Songs[0] = '<a href="<a href="https://youtu.be/gsVGf1T2Hfs" data-lity>Dragon Night / <br class="pc-dnone">SEKAI NO OWARI</a>';
+    h27Songs[1] = '<a href="https://youtu.be/7gcCRAl58u4" data-lity>SUN / <br class="pc-dnone">星野源</a>';
+    h27Songs[2] = '<a href="https://youtu.be/LIlZCmETvsY" data-lity>新宝島 / <br class="pc-dnone">サカナクション</a>';
+    h27Song = document.getElementById("h27Song");
+    h27Song.innerHTML = h27Songs[x];
+
+    // 2016年
+    h28Songs[0] = '<a href="https://youtu.be/jhOVibLEDhA" data-lity>恋 / <br class="pc-dnone">星野源</a>';
+    h28Songs[1] = '<a href="https://youtu.be/PDSkFeMVNFs" data-lity>前前前世 / <br class="pc-dnone">RADWIMPS</a>';
+    h28Songs[2] = '<a href="https://youtu.be/DeGkiItB9d8" data-lity>サイレントマジョリティー / <br class="pc-dnone">欅坂46</a>';
+    h28Song = document.getElementById("h28Song");
+    h28Song.innerHTML = h28Songs[x];
+
+    // 2017年
+    h29Songs[0] = '<a href="https://youtu.be/r4SdiT7mm7Y" data-lity>インフルエンサー / <br class="pc-dnone">乃木坂46</a>';
+    h29Songs[1] = '<a href="https://youtu.be/3yoJY0IqiV0" data-lity>感情のピクセル / <br class="pc-dnone">岡崎体育</a>';
+    h29Songs[2] = '<a href="https://youtu.be/-tKVN2mAKRI" data-lity>打上花火 / <br class="pc-dnone">DAOKO × 米津玄師</a>';
+    h29Song = document.getElementById("h29Song");
+    h29Song.innerHTML = h29Songs[x];
+
+    // 2018年
+    h30Songs[0] = '<a href="https://youtu.be/EHw005ZqCXk" data-lity>ノーダウト / <br class="pc-dnone">Official髭男dism</a>';
+    h30Songs[1] = '<a href="https://youtu.be/SX_ViT4Ra7k" data-lity>Lemon / <br class="pc-dnone">米津玄師</a>';
+    h30Songs[2] = '<a href="https://youtu.be/sr--GVIoluU" data-lity>U.S.A. /<br class="pc-dnone">DA PUMP</a>';
+    h30Song = document.getElementById("h30Song");
+    h30Song.innerHTML = h30Songs[x];
+    
+    // 2019年
+    h31Songs[0] = '<a href="https://youtu.be/ony539T074w" data-lity>白日 / <br class="pc-dnone">King Gnu</a>';
+    h31Songs[1] = '<a href="https://youtu.be/0xSiBpUdW4E" data-lity>マリーゴールド / <br class="pc-dnone">あいみょん</a>';
+    h31Songs[2] = '<a href="https://youtu.be/7940nuwCEYA" data-lity>まちがいさがし /<br class="pc-dnone">菅田将暉</a>';
+    h31Song = document.getElementById("h31Song");
+    h31Song.innerHTML = h31Songs[x];
+});
